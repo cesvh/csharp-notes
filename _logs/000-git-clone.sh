a9e9be5f93a711807b58a3548d06bfc0493eb6d3
cesvh <cesar.vega.hdez@gmail.com>
2025-11-29 02:03:50 -0600

    feat: git clone repository

---
+++ _logs/000-git-clone.sh
@@ -0,0 +1,30 @@
+cesvh@cesvh MINGW64 ~/dev/github
+$ git clone https://github.com/cesvh/csharp-notes.git
+Cloning into 'csharp-notes'...
+remote: Enumerating objects: 5, done.
+remote: Counting objects: 100% (5/5), done.
+remote: Compressing objects: 100% (5/5), done.
+remote: Total 5 (delta 0), reused 0 (delta 0), pack-reused 0 (from 0)
+Receiving objects: 100% (5/5), done.
+
+cesvh@cesvh MINGW64 ~/dev/github
+$ cd csharp-notes/
+
+cesvh@cesvh MINGW64 ~/dev/github/csharp-notes (main)
+$ ls -l
+total 5
+-rw-r--r-- 1 cesvh 197609 1083 Nov 29 01:44 LICENSE
+-rw-r--r-- 1 cesvh 197609   74 Nov 29 01:44 README.md
+
+cesvh@cesvh MINGW64 ~/dev/github/csharp-notes (main)
+$ ls -la
+total 21
+drwxr-xr-x 1 cesvh 197609    0 Nov 29 01:44 ./
+drwxr-xr-x 1 cesvh 197609    0 Nov 29 01:44 ../
+drwxr-xr-x 1 cesvh 197609    0 Nov 29 01:44 .git/
+-rw-r--r-- 1 cesvh 197609  984 Nov 29 01:44 .gitignore
+-rw-r--r-- 1 cesvh 197609 1083 Nov 29 01:44 LICENSE
+-rw-r--r-- 1 cesvh 197609   74 Nov 29 01:44 README.md
+
+cesvh@cesvh MINGW64 ~/dev/github/csharp-notes (main)
+$
